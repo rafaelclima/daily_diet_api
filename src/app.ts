244cc221ac879849611f3c5264324523dab24e83
Rafael Lima <rafaelclima.ti@gmail.com>
2025-02-13 17:23:51 -0300

    add pino-pretty para melhorar o log do fastify

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 
-import fastify from 'fastify';
 import fastifyCookie from '@fastify/cookie';
 import fastifySession from '@fastify/session';
+import fastify from 'fastify';
 import { login } from './routes/login';
 import { mealsRoutes } from './routes/meals';
 import { usersRoutes } from './routes/users';
@@ -11,7 +11,17 @@ if (!process.env.SESSION_SECRET) {
   throw new Error('A variável de ambiente SESSION_SECRET não está definida.');
 }
 
-export const app = fastify();
+export const app = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname'
+      }
+    }
+  }
+});
 
 app.register(fastifyCookie);
 
