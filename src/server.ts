db7a110609f9a43832f1072936f8872a74793fcd
rafaelclima <rafaelclima.ti@gmail.com>
2025-03-10 16:12:26 -0300

    feat: alterado a porta para pegar do arquivo env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { app } from './app';
+import { env } from './env';
 
 // Iniciando o servidor
-app.listen({ port: 3200 }, (err, address) => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
